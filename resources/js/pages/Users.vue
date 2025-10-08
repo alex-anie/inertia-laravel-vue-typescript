c634ec383d1c6a1b78b80f37517c489c66872c7a
alex-anie <ocxigin@gmail.com>
2025-10-08 18:16:33 +0100

    Search filled implemented

--- resources/js/pages/Users.vue
+++ resources/js/pages/Users.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
     import Layouts from '@/Shared/Layouts.vue';
-    import Paginator, {PaginationLink} from '@/Shared/Paginator.vue';
-    import { Link, Head } from '@inertiajs/vue3';
-
+    import Paginator, { PaginationLink } from '@/Shared/Paginator.vue';
+    import { Link, Head, router } from '@inertiajs/vue3';
+    import { ref, watch } from 'vue';
+    
     defineOptions({
         layout: Layouts
     });
@@ -17,15 +18,37 @@
         links: PaginationLink[]
     }
 
-    defineProps<{
-        users: Paginated<User>
+    interface Filter {
+        search?: string
+    }
+
+    const props = defineProps<{
+        users: Paginated<User>,
+        filters: Filter
     }>()
+
+    // bind initial search value from backend
+    const search = ref(props.filters.search || '');
+
+    watch(search, (value) => {
+        router.get('/users',
+            {search: value},
+            {
+                preserveState: true,
+                replace: true
+            }
+        )
+    });
 
 </script>
 
 <template>
     <Head title="my users" />
-    <h1 class="text-2xl mb-4">Users</h1>
+
+    <div class="flex justify-between mb-4">
+        <h1 class="text-2xl">Users</h1>
+        <input v-model="search" type="text" placeholder="Sweater..." class="border px-2 rounded-2xl"/>
+    </div>
 
     <table class="min-w-full divide-y divide-gray-200 border">
     <tbody class="divide-y divide-gray-200">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 // Route::get('/', function () {
 //     return Inertia::render('Welcome');
@@ -12,17 +13,25 @@ Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
 
-Route::get('/users', function () {
-    // return User::paginate(10);
+Route::get('/users', function (Request $request) {
+    $users = User::query()
+        ->when($request->input('search'), function ($query, $search) {
+            $query->where('name', 'like', "%{$search}%");
+        })
+        ->paginate(10)
+        ->withQueryString()
+        ->through(fn ($user) => [
+            'id' => $user->id,
+            'name' => $user->name,
+        ]);
+
     return Inertia::render('Users', [
-        'users' => User::paginate(10)->through(fn($user)=> [
-            'id' => $user-> id,
-            'name' => $user-> name
-        ])
+        'users' => $users,
+        'filters' => $request->only(['search']),
     ]);
 })->name('users');
 
-Route::get('/settings', function () {
+Route::get( '/settings', function () {
     return Inertia::render('Settings');
 })->name('settings');
 
